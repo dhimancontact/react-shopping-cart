cc63a53e388b0329f76557a175b4778053e08738
Anil Kumar <anilkumar.dhiman1@gmail.com>
2021-06-05 19:23:02 +0530

    redux with create order is done

---
+++ src/actions/orderActions.js
@@ -0,0 +1,21 @@
+import { CLEAR_CART, CLEAR_ORDER, CREATE_ORDER } from "../types";
+
+export const createOrder = (order) => (dispatch) => {
+  fetch("http://localhost:3001/api/orders", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(order),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      dispatch({ type: CREATE_ORDER, payload: data });
+      // localStorage.clear("clearItems");
+      // dispatch({ type: CLEAR_CART });
+    });
+};
+
+export const clearOrder = () => (dispatch) => {
+  dispatch({ type: CLEAR_ORDER });
+};
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,6 +3,9 @@ import formatCurrency from "../util/util";
 import Fade from "react-reveal/Fade";
 import { connect } from "react-redux";
 import { removeCartItem } from "../actions/cartActions";
+import { createOrder, clearOrder } from "../actions/orderActions";
+import Modal from "react-modal";
+import Zoom from "react-reveal/Zoom";
 
 class Cart extends Component {
   constructor(props) {
@@ -26,13 +29,20 @@ class Cart extends Component {
       email: this.state.email,
       address: this.state.address,
       cartItems: this.props.cartItems,
+      total: this.props.cartItems.reduce((a, c) => a + c.price * c.count, 0),
     };
 
     this.props.createOrder(order);
   };
 
+  closeModal = () => {
+    this.props.clearOrder();
+  };
+
   render() {
-    const { cartItems } = this.props;
+    const { cartItems, order } = this.props;
+    console.log(this.props.order);
+
     return (
       <div>
         {cartItems.length === 0 ? (
@@ -41,6 +51,48 @@ class Cart extends Component {
           <div className="cart cart-header">
             You have {cartItems.length} in the cart{" "}
           </div>
+        )}
+
+        {order && (
+          <Modal isOpen={true} onRequestClose={this.closeModal}>
+            <Zoom>
+              <button className="close=modal" onClick={this.closeModal}>
+                X
+              </button>
+              <div className="order-details">
+                <h3 className="success-message">Your order has been placed</h3>
+                <h2>Order {order._id}</h2>
+                <ul>
+                  <li>
+                    <div>Name:</div>
+                    <div>{order.name}</div>
+                  </li>
+                  <li>
+                    <div>Email:</div>
+                    <div>{order.email}</div>
+                  </li>
+                  <li>
+                    <div>Address:</div>
+                    <div>{order.address}</div>
+                  </li>
+                  <li>
+                    <div>Total:</div>
+                    <div>{order.total}</div>
+                  </li>
+                  <li>
+                    <div>Cart Items:</div>
+                    <div>
+                      {order.cartItems.map((x) => (
+                        <div key={x._id}>
+                          {x.count} * {x.title}
+                        </div>
+                      ))}
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </Zoom>
+          </Modal>
         )}
         <div className="cart">
           <Fade left cascade>
@@ -136,7 +188,12 @@ class Cart extends Component {
 
 export default connect(
   (state) => ({
+    order: state.order.order,
     cartItems: state.cart.cartItems,
   }),
-  { removeCartItem }
+  {
+    removeCartItem,
+    createOrder,
+    clearOrder,
+  }
 )(Cart);
